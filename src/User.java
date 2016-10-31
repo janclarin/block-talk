6ee51dc2a033dbc310fb5031c06b2d21e4d26db0
Jan Clarin <hi@janclarin.com>
2016-10-31 11:21:22 -0600

    Add user fields and constructor

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.net.InetAddress;
+
 /**
  * This class represents an end user and all information
  * relevant to communicating with them, including username, 
@@ -9,4 +11,40 @@
  */
 public class User
 {
+	/**
+	 * User IP address. Should not change after initialization.
+	 */
+	private final InetAddress ipAddress;
+	
+	/**
+	 * Port number. Should not change after initialization.
+	 */
+	private final short port;
+	
+	/**
+	 * Creates a new User with the given IP address and port. 
+	 * @param ipAddress IP address.
+	 * @param port Port number.
+	 */
+	public User(InetAddress ipAddress, short port)
+	{
+		this.ipAddress = ipAddress;
+		this.port = port;
+	}
+	
+	/**
+	 * Gets the IP address of this User.
+	 * @return The IP address of this User. 
+	 */
+	public InetAddress getIpAddress() {
+		return ipAddress;
+	}
+	
+	/**
+	 * Gets the port of this User.
+	 * @return The port of this User.
+	 */
+	public short getPort() {
+		return port;
+	}
 }
