86a5145b37d42f7961198cee62eca3c43dc03468
Sir Cinnamon <sircinnamon@gmail.com>
2016-10-31 10:07:28 -0600

    Add javadoc for User.java

--- User.java
+++ User.java
@@ -1,3 +1,12 @@
+/**
+ * This class represents an end user and all information
+ * relevant to communicating with them, including username, 
+ * IP, port, and public key.
+ *
+ * @author Clinton Cabiles
+ * @author Jan Clarin
+ * @author Riley Lahd
+ */
 public class User
 {
 }
