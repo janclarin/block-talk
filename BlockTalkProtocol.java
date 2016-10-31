049296c90061956e499488ee795d6ae3f3b70cac
Sir Cinnamon <sircinnamon@gmail.com>
2016-10-31 10:04:47 -0600

    Add class files to be filled.

---
+++ BlockTalkProtocol.java
@@ -0,0 +1,13 @@
+/**
+ * This class manages the sending and receiveing of messages using
+ * the prescribed format. It is intended to be used for both Client-Server
+ * and P2P transmission. It also handles the encrypting and decrypting
+ * of messages.
+ *
+ * @author Clinton Cabiles
+ * @author Jan Clarin
+ * @author Riley Lahd
+ */
+public class BlockTalkProtocol
+{
+}
---
+++ Server.java
@@ -0,0 +1,11 @@
+/**
+ * This class represents a head server of the system for exchanging
+ * keys and IPs.
+ *
+ * @author Clinton Cabiles
+ * @author Jan Clarin
+ * @author Riley Lahd
+ */
+public class Server
+{
+}
