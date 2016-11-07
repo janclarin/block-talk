2f8ad802b12e47753567ebc4dd771c1addd2dd2e
Jan Clarin <hi@janclarin.com>
2016-11-07 13:43:13 -0700

    Add username field

--- src/models/User.java
+++ src/models/User.java
@@ -12,8 +12,13 @@ import java.net.InetAddress;
  * @author Riley Lahd
  */
 public class User {
+    /**
+     * Username. Should not change after initialization.
+     */
+    private final String username;
+
 	/**
-	 * models.User IP address. Should not change after initialization.
+	 * IP address. Should not change after initialization.
 	 */
 	private final InetAddress ipAddress;
 	
@@ -23,36 +28,46 @@ public class User {
 	private final int port;
 	
 	/**
-	 * Creates a new models.User with the given IP address and port.
+	 * Creates a new User with the given username, IP address, and port.
+     * @param username Username.
 	 * @param ipAddress IP address.
 	 * @param port Port number.
 	 */
-	public User(InetAddress ipAddress, int port) {
+	public User(String username, InetAddress ipAddress, int port) {
+        this.username = username;
 		this.ipAddress = ipAddress;
 		this.port = port;
 	}
+
+    /**
+     * Gets the username of this User.
+     * @return The username of this User.
+     */
+	public String getUsername() {
+        return username;
+    }
 	
 	/**
-	 * Gets the IP address of this models.User.
-	 * @return The IP address of this models.User.
+	 * Gets the IP address of this User.
+	 * @return The IP address of this User.
 	 */
 	public InetAddress getIpAddress() {
 		return ipAddress;
 	}
 	
 	/**
-	 * Gets the port of this models.User.
-	 * @return The port of this models.User.
+	 * Gets the port of this User.
+	 * @return The port of this User.
 	 */
 	public int getPort() {
 		return port;
 	}
 	
 	/**
-	 * Returns a String representation of this models.User.
-	 * @return String representation of this models.User.
+	 * Returns a String representation of this User.
+	 * @return String representation of this User.
 	 */
 	public String toString() {
-		return String.format("IP %s Port %d", ipAddress, port);
+		return String.format("(Username %s | IP %s | Port %d)", username, ipAddress, port);
 	}
 }
