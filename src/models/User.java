61f32e29b354521ab30cfd365fa9951cf01f2eeb
Jan Clarin <hi@janclarin.com>
2016-11-07 17:41:18 -0700

    Add username and fix spacing

--- src/models/User.java
+++ src/models/User.java
@@ -4,7 +4,7 @@ import java.net.InetAddress;
 
 /**
  * This class represents an end user and all information
- * relevant to communicating with them, including username, 
+ * relevant to communicating with them, including username,
  * IP, port, and public key.
  *
  * @author Clinton Cabiles
@@ -17,57 +17,62 @@ public class User {
      */
     private final String username;
 
-	/**
-	 * IP address. Should not change after initialization.
-	 */
-	private final InetAddress ipAddress;
-	
-	/**
-	 * Port number. Should not change after initialization.
-	 */
-	private final int port;
-	
-	/**
-	 * Creates a new User with the given username, IP address, and port.
-     * @param username Username.
-	 * @param ipAddress IP address.
-	 * @param port Port number.
-	 */
-	public User(String username, InetAddress ipAddress, int port) {
+    /**
+     * IP address. Should not change after initialization.
+     */
+    private final InetAddress ipAddress;
+
+    /**
+     * Port number. Should not change after initialization.
+     */
+    private final int port;
+
+    /**
+     * Creates a new User with the given username, IP address, and port.
+     *
+     * @param username  Username.
+     * @param ipAddress IP address.
+     * @param port      Port number.
+     */
+    public User(String username, InetAddress ipAddress, int port) {
         this.username = username;
-		this.ipAddress = ipAddress;
-		this.port = port;
-	}
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
 
     /**
      * Gets the username of this User.
+     *
      * @return The username of this User.
      */
-	public String getUsername() {
+    public String getUsername() {
         return username;
     }
-	
-	/**
-	 * Gets the IP address of this User.
-	 * @return The IP address of this User.
-	 */
-	public InetAddress getIpAddress() {
-		return ipAddress;
-	}
-	
-	/**
-	 * Gets the port of this User.
-	 * @return The port of this User.
-	 */
-	public int getPort() {
-		return port;
-	}
-	
-	/**
-	 * Returns a String representation of this User.
-	 * @return String representation of this User.
-	 */
-	public String toString() {
-		return String.format("%s | %s:%d", username, ipAddress, port);
-	}
+
+    /**
+     * Gets the IP address of this User.
+     *
+     * @return The IP address of this User.
+     */
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    /**
+     * Gets the port of this User.
+     *
+     * @return The port of this User.
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * Returns a String representation of this User.
+     *
+     * @return String representation of this User.
+     */
+    public String toString() {
+        return String.format("%s | %s:%d", username, ipAddress, port);
+    }
 }
