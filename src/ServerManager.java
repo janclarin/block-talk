b7a05d1918fe802e8bff1ec4ca116545249cbabf
ccabiles <clin.cabiles@gmail.com>
2016-11-09 17:09:56 -0700

    Add server list and iterate through each server

--- src/ServerManager.java
+++ src/ServerManager.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -14,9 +15,10 @@ import java.util.concurrent.Executors;
  */
 public class ServerManager {
 	
-	private static List<ServerSocket> servers;
+	private static List<ClientConnectionListener> servers;
 	private static int port = 9999;
 	private static boolean listen;
+	private static ClientConnectionListener server;
 	
 	/**
 	 * Starts the main server manager.
@@ -28,13 +30,15 @@ public class ServerManager {
 		try{
 			ExecutorService threadPool = Executors.newCachedThreadPool();
 			port = Integer.parseInt(args[0]);
-			servers = new ArrayList<ServerSocket>();
+			servers = new ArrayList<ClientConnectionListener>();
 			ServerSocket listenServer = new ServerSocket(port);
 			
+			//use one main server for now
+			servers.add(new Server());
+			
 			while(listen){
-				//Socket activeSocket = listenServer.accept();
-				for(ServerSocket server: servers){
-					threadPool.execute(new ClientConnection(server.accept(), new Server()));
+				for(ClientConnectionListener listener : servers){
+					threadPool.execute(new ClientConnection(listenServer.accept(), listener));
 				}
 			}
 		}
